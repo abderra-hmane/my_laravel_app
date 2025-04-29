72735059a75102b743ea8c8117e1cb65660f7519
abderra-hmane <abderrahmanewinz@gmail.com>
2025-04-29 22:18:15 +0100

    Theme Of the Project .

---
+++ resources/views/theme/master.blade.php
@@ -0,0 +1,11 @@
+@include('theme.partials.head')
+
+<body>
+@include('theme.partials.header')
+
+
+@yield('content')
+
+<!--================ Start Footer Area =================-->
+@include('theme.partials.footer')
+<!--================ End Footer Area =================-->
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ThemeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*ThemeController*/
 
+Route::controller(ThemeController::class)->name('theme.')->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/category', 'category')->name('category');
+    Route::get('/contact', 'contact')->name('contact');
+    Route::get('/blogs', 'blogs')->name('blogs');
+    Route::get('/register', 'register')->name('register');
+    Route::get('/login', 'login')->name('login');
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -17,4 +24,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
